1b8122b79dd844ccb7a7dcd3d3d433974bb402cb
Xiaolong Zhang <xlzhsteven@gmail.com>
2014-04-01 21:55:04 -0700

    Blinders On!
    The ranger-devs also want to upgrade Lighthouse Rock with some super-blinding light bulbs, while sending the existing bulbs to be used at the Underwater Outpost. Below is the Lighthouse Rock object, and an array of super-blinders. Each index of the superblinder array contains a bulb name and its wattage within a subarray. Do the following:
    
    Completely remove the existing bulb array from the Lighthouse Rock object, including its property.
    Add in the new array using only its variable name and the property name weaponBulbs.
    Examine the array and write a line of code that logs out the name of the superblinder with the highest wattage using only object and array notation (no hard-coding the message with the name).

--- buildingObject.js
+++ buildingObject.js
@@ -1,17 +1,13 @@
 /**
  * Created by Xiaolong on 4/1/14.
  */
-var vehicle1 = {type: "Motorboat", capacity: 6, storedAt: "Ammunition Depot"};
-var vehicle2 = {type: "Jet Ski", capacity: 1, storedAt: "Reef Dock"};
-var vehicle3 = {type: "Submarine", capacity: 8, storedAt: "Underwater Outpost"};
-vehicle1.capacity += 4;
-vehicle1.submersible = false;
-vehicle1.weapon = "Rear-Mounted Slingshot";
-vehicle1["# of weapons"] = 1;
-vehicle2.submersible = false;
-vehicle2.weapon = "Lasers";
-vehicle2["# of weapons"] = 4;
-vehicle3.weapon = "Torpedoes";
-vehicle3.capacity *= 2;
-vehicle3.submersible = true;
-vehicle3["# of weapons"] = 8;
+var superBlinders = [ ["Firelight", 4000], ["Solar Death Ray", 6000], ["Supernova", 12000] ];
+var lighthouseRock = {
+    gateClosed: true,
+    bulbs: [ 200, 500, 750 ],
+    capacity: 30,
+    secretPassageTo: "Underwater Outpost"
+};
+delete lighthouseRock.bulbs;
+lighthouseRock.weaponBulbs = superBlinders;
+console.log(lighthouseRock.weaponBulbs[2][0]);
