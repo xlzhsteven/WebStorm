150ca5ae2b94ae59c81c30be7c9412d6fb2f3d23
Xiaolong Zhang <xlzhsteven@gmail.com>
2014-04-01 21:37:08 -0700

    Building Objects 2

--- buildingObject.js
+++ buildingObject.js
@@ -4,12 +4,12 @@
 var vehicle1 = {type: "Motorboat", capacity: 6, storedAt: "Ammunition Depot"};
 var vehicle2 = {type: "Jet Ski", capacity: 1, storedAt: "Reef Dock"};
 var vehicle3 = {type: "Submarine", capacity: 8, storedAt: "Underwater Outpost"};
-var vehicles = [vehicle1, vehicle2, vehicle3];
-var findVehicle = function(name, list){
-    for(var i = 0; i < list.length; i++){
-        if(name===list[i].type){
-            return list[i].storedAt;
-        }
-    }
-};
-findVehicle("Submarine", vehicles);
+vehicle1.capacity += 4;
+vehicle1.submersible = false;
+vehicle1.weapon = "Rear-Mounted Slingshot";
+vehicle2.submersible = false;
+vehicle2.weapon = "Lasers";
+vehicle3.weapon = "Torpedoes";
+vehicle3.capacity *= 2;
+vehicle3.submersible = true;
+console.log(vehicle2);
