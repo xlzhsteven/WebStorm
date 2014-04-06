97e3be958b9eafb0ced1bca7a56572ae51be9b78
Zhang, Xiaolong <xiaolozhang@paypal.com>
2014-04-06 12:11:07 -0700

    Enumeration I
    The devs at Lighthouse Rock have received a shipment of Spearguns for use in the battle against the Really Awesome Pirates (RAP). They need to know quickly on their augmented reality eyeglasses just which spears have arrived (they can’t leave their posts at the bulbs). The Object for the shipping box is below:
    
    var rockSpearguns = {
      Sharpshooter: {barbs: 2, weight: 10, heft: "overhand"},
      Pokepistol: {barbs: 4, weight: 8, heft: "shoulder"},
      Javelinjet: {barbs: 4, weight: 12, heft: "waist"},
      Firefork: {barbs: 6, weight: 8, heft: "overhand"},
      "The Impaler": {barbs: 1, weight: 30, heft: "chest"}
    };
    Build a declared function called listGuns that accepts a container of spearguns, called guns, as a parameter and logs out the name of each Speargun. Additionally, use speargun to refer to the property names inside your function. When your function is built, call it on the given shipment of guns.

--- buildingObject.js
+++ buildingObject.js
@@ -1,31 +1,16 @@
 /**
  * Created by Xiaolong on 4/1/14.
  */
-var vehicle3 = {
-    type: "Submarine", capacity: 8, storedAt: "Underwater Outpost",
-    ranger1: { name: "Gregg Pollack", skillz: "Lasering", dayOff: "Friday"},
-    ranger2: { name: "Bijan Boustani", skillz: "Roundhouse Kicks", dayOff: "Tuesday"},
-    ranger3: { name: "Ashley Smith", skillz: "Torpedoing", dayOff: "Friday"},
-    ranger4: { name: "Mark Krupinski", skillz: "Sniping", dayOff: "Wednesday"},
-    numRangers: 4
+var rockSpearguns = {
+    Sharpshooter: {barbs: 2, weight: 10, heft: "overhand"},
+    Pokepistol: {barbs: 4, weight: 8, heft: "shoulder"},
+    Javelinjet: {barbs: 4, weight: 12, heft: "waist"},
+    Firefork: {barbs: 6, weight: 8, heft: "overhand"},
+    "The Impaler": {barbs: 1, weight: 30, heft: "chest"}
 };
 
-function relieveDuty (vehicle, day){
-    var offDuty = [];
-    var onDuty = [];
-    for(var i = 1; i <= vehicle["numRangers"]; i++){
-        if(vehicle["ranger"+i]["dayOff"]===day){
-            offDuty.push(vehicle["ranger"+i]);
-            vehicle["numRangers"] -= 1;
-        } else{
-            onDuty.push(vehicle["ranger"+i]);
-        }
-        delete vehicle["ranger"+i];
+function listGuns(guns){
+    for (speargun in guns){
+        console.log(speargun);
     }
-    for(var j = 1; j <= onDuty.length; j++){
-        vehicle["ranger"+j] = onDuty[j-1];
-    }
-    return offDuty;
 }
-
-var offToday = relieveDuty(vehicle3, "Friday");
