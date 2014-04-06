fc922522c00a99b214c057960ca633261dd5ccc6
Zhang, Xiaolong <xiaolozhang@paypal.com>
2014-04-06 09:21:48 -0700

    Functions As Properties I To improve organization and memory efficiency, the ranger-devs have asked you to make all functions that add objects to objects part of the destination location instead of being present in the global scope.
    Take the addRanger function that was designed for lighthouseRock, refactor it, and add it directly within the lighthouseRock object. The property name that you use in lighthouseRock should also be addRanger. The addRanger declared function is provided for you below.
    
    function addRanger(location, name, skillz, station){
      location.numRangers++;
      location["ranger" + location.numRangers] = {
        name: name,
        skillz: skillz,
        station: station
      };
    }

--- buildingObject.js
+++ buildingObject.js
@@ -11,15 +11,8 @@ var lighthouseRock = {
     ranger1: {name: "Nick Walsh", skillz: "magnification burn", station: 2},
     ranger2: {name: "Drew Barontini", skillz: "uppercut launch", station: 3},
     ranger3: {name: "Christine Wong", skillz: "bomb defusing", station: 1}
-};
-function dontPanic (location){
-    var list = "";
-    for(var i = 1; i<=location.numRangers; i++){
-        list = list + location["ranger" + i].name + ", man the " +
-            location.weaponBulbs[location["ranger"+i].station-1][0] +
-            "!\n";
+    addRanger: function(name, skillz, station){
+        this.numRangers++;
+        this["ranger" + this.numRangers] = {name: name, skillz: skillz, station: station};
     }
-    alert("Avast, me hearties!\n" +
-        "There be Pirates nearby! Stations!\n" + list);
-}
-dontPanic(lighthouseRock);
+};
