7853ea5fe301238417aef1d65fcb05729788606a
Zhang, Xiaolong <xiaolozhang@paypal.com>
2014-04-06 20:00:55 -0700

    Prototypes IV
    Welp, it’s time to figure out which cows are in line for a first breeding. Your awesome prototype skills are making life easy on the cowboy-devs, so easy that they’ve asked you to use your own new functions to find out how many cows haven’t had calves yet out of the lists you’ll find below. Calculate this amount and store it in a variable called numPriorityCows. Alert the following message to the screen:
    
    Herd-merger has indicated <number> cows of top breeding priority.
    Your previously built prototypal functions are provided for your reference.
    
    var canyonCows = [
      {name: "Bessie", type: "cow", hadCalf: "Burt"},
      {name: "Donald", type: "bull", hadCalf: null},
      {name: "Esther", type: "calf", hadCalf: null},
      {name: "Burt", type: "calf", hadCalf: null},
      {name: "Sarah", type: "cow", hadCalf: "Esther"},
      {name: "Samson", type: "bull", hadCalf: null},
      {name: "Delilah", type: "cow", hadCalf: null}
    ];
    
    var valleyCows = [
      {name: "Danielle", type: "cow", hadCalf: null},
      {name: "Brittany", type: "cow", hadCalf: "Christina"},
      {name: "Jordan", type: "bull", hadCalf: null},
      {name: "Trevor", type: "bull", hadCalf: null},
      {name: "Christina", type: "calf", hadCalf: null},
      {name: "Lucas", type: "bull", hadCalf: null}
    ];
    
    var forestCows = [
      {name: "Legolas", type: "calf", hadCalf: null},
      {name: "Gimli", type: "bull", hadCalf: null},
      {name: "Arwen", type: "cow", hadCalf: null},
      {name: "Galadriel", type: "cow", hadCalf: null},
      {name: "Eowyn", type: "cow", hadCalf: "Legolas"}
    ];
    
    var badlandsCows = [
      {name: "Voldemort", type: "bull", hadCalf: null},
      {name: "Maleficent", type: "cow", hadCalf: null},
      {name: "Ursula", type: "cow", hadCalf: "Draco"},
      {name: "Draco", type: "calf", hadCalf: null},
      {name: "Joker", type: "bull", hadCalf: null},
      {name: "Chucky", type: "calf", hadCalf: null},
      {name: "Samara", type: "cow", hadCalf: "Chucky"}
    ];

--- buildingObject.js
+++ buildingObject.js
@@ -1,6 +1,42 @@
 /**
  * Created by Xiaolong on 4/1/14.
  */
+var canyonCows = [
+    {name: "Bessie", type: "cow", hadCalf: "Burt"},
+    {name: "Donald", type: "bull", hadCalf: null},
+    {name: "Esther", type: "calf", hadCalf: null},
+    {name: "Burt", type: "calf", hadCalf: null},
+    {name: "Sarah", type: "cow", hadCalf: "Esther"},
+    {name: "Samson", type: "bull", hadCalf: null},
+    {name: "Delilah", type: "cow", hadCalf: null}
+];
+
+var valleyCows = [
+    {name: "Danielle", type: "cow", hadCalf: null},
+    {name: "Brittany", type: "cow", hadCalf: "Christina"},
+    {name: "Jordan", type: "bull", hadCalf: null},
+    {name: "Trevor", type: "bull", hadCalf: null},
+    {name: "Christina", type: "calf", hadCalf: null},
+    {name: "Lucas", type: "bull", hadCalf: null}
+];
+
+var forestCows = [
+    {name: "Legolas", type: "calf", hadCalf: null},
+    {name: "Gimli", type: "bull", hadCalf: null},
+    {name: "Arwen", type: "cow", hadCalf: null},
+    {name: "Galadriel", type: "cow", hadCalf: null},
+    {name: "Eowyn", type: "cow", hadCalf: "Legolas"}
+];
+
+var badlandsCows = [
+    {name: "Voldemort", type: "bull", hadCalf: null},
+    {name: "Maleficent", type: "cow", hadCalf: null},
+    {name: "Ursula", type: "cow", hadCalf: "Draco"},
+    {name: "Draco", type: "calf", hadCalf: null},
+    {name: "Joker", type: "bull", hadCalf: null},
+    {name: "Chucky", type: "calf", hadCalf: null},
+    {name: "Samara", type: "cow", hadCalf: "Chucky"}
+];
 Object.prototype.noCalvesYet = function (){
     if(this.hadCalf === null && this.type === "cow"){
         return true;
@@ -8,7 +44,6 @@ Object.prototype.noCalvesYet = function (){
         return false;
     }
 };
-
 Array.prototype.countForBreeding = function(){
     var numToBreed = 0;
     for(var i = 0; i < this.length; i++){
@@ -18,3 +53,7 @@ Array.prototype.countForBreeding = function(){
     }
     return numToBreed;
 };
+
+var numPriorityCows = canyonCows.countForBreeding() + valleyCows.countForBreeding() + forestCows.countForBreeding() + badlandsCows.countForBreeding();
+
+console.log("Herd-merger has indicated " + numPriorityCows + " cows of top breeding priority.");
