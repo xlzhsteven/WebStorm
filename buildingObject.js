7511ea28fece7a5830b8e4d261ac80e4f5f1bbcb
Zhang, Xiaolong <xiaolozhang@paypal.com>
2014-04-06 21:35:24 -0700

    Creation with Prototypes II
    Turns out that there are few fence posts that have some special properties. Some have weathervanes, some have birdhouses, and some even have emergency lights.
    
    Below are the data for three posts that need to be created using the genericPost as a prototype (as in the last challenge). The cowboys have given you all the data you’ll need to build each using the prototype and then assign unique property values through modification. Call each of your posts post<number>, just like you did in the last challenge.
    
    x: 0, y: -3,
    postNum: 8,
    connectionsTo: 10
    
    x: 6, y: 8,
    postNum: 9,
    connectionsTo: 10
    
    x: -2, y: 3,
    postNum: 10,
    connectionsTo: 8, 9
    
    The cowboy-devs have prepared a list of additions that need to happen for certain special fence posts. After you’ve built the above three posts, add properties to those post where the cowboy-devs have deemed appropriate.
    
    Any fence posts with an even ‘y’ coordinate have a birdhouse, and therefore have a numBirds property initially set to 0.
    Any fence posts connected to Post #9, but are not Post #9, have a property of weathervane initially set to “N”.
    Even numbered fence posts have emergency lights, and a lightsOn property initially set to false.
    The base fencepost is again provided for your reference. There are many single lines of code to be entered on this one, so be careful to assign all properties necessary.

--- buildingObject.js
+++ buildingObject.js
@@ -1,8 +1,10 @@
 /**
  * Created by Xiaolong on 4/1/14.
  */
-var genericPost =
-{x: 0, y: 0, postNum: undefined,
+var genericPost = {
+    x: 0,
+    y: 0,
+    postNum: undefined,
     connectionsTo: undefined,
     sendRopeTo: function ( connectedPost ) {
         if(this.connectionsTo == undefined){
@@ -14,15 +16,23 @@ var genericPost =
         }
     }
 };
-var post1 = Object.create(genericPost);
-var post2 = Object.create(genericPost);
-
-post1.x = -2;
-post1.y = 4;
-post1.postNum = 1;
-
-post2.x = 5;
-post2.y = 1;
-post2.postNum = 2;
-
-post1.sendRopeTo(post2);
+var post8 = Object.create(genericPost);
+var post9 = Object.create(genericPost);
+var post10 = Object.create(genericPost);
+post8.x = 0;
+post8.y = -3;
+post8.postNum = 8;
+post8.sendRopeTo(post10);
+post9.x = 6;
+post9.y = 8;
+post9.postNum = 9;
+post9.sendRopeTo(post10);
+post10.x = -2;
+post10.y = 3;
+post10.postNum = 10;
+post10.sendRopeTo(post8);
+post10.sendRopeTo(post9);
+post9.numBirds = 0;
+post10.weathervane = "N";
+post8.lightsOn = false;
+post10.lightsOn = false;
