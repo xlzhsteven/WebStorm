34018487bfff6de4906bf7b6ae083093c6b1cafb
Zhang, Xiaolong <xiaolozhang@paypal.com>
2014-04-07 20:30:44 -0700

    Constructor II
    Now that you’ve got a Fencepost constructor, let’s make some posts!
    
    The cowboy-devs need three new posts for the Plains, described below:
    
    x: -3, y: 4
    numPost: 18
    
    x: 5, y: -1,
    numPost: 19
    
    x: -2, y: 10
    numPost: 20.
    
    Use your constructor to make these fence posts, naming them post<number> as before. Then connect them with the connection function using the following criteria.
    
    If two posts both have even y-coordinates, they should be connected.
    If two posts both have odd x-coordinates, they should be connected.
    Your constructor is provided for you.

--- buildingObject.js
+++ buildingObject.js
@@ -10,3 +10,12 @@ function Fencepost (x, y, postNum){
         this.connectionsTo.push(connectedPost);
     };
 }
+
+var post18 = new Fencepost(-3, 4, 18);
+var post19 = new Fencepost(5, -1, 19);
+var post20 = new Fencepost(-2, 10, 20);
+
+post18.sendRopeTo(post20);
+post20.sendRopeTo(post18);
+post18.sendRopeTo(post19);
+post19.sendRopeTo(post18);
