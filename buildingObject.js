3b64dc89f706d7555b796ec6a7637a099380d33c
Zhang, Xiaolong <xiaolozhang@paypal.com>
2014-04-07 21:50:14 -0700

    Overriding Prototypal Methods II
    Next up, the Plains of Prototypes cowboy-devs have asked for an improved toString method for their fence posts. An example of the string message they’d like is below:
    
    Fence post #10:
    Connected to posts:
    11
    12
    13
    Distance from ranch: 5 yards
    Override the toString method by using the Fencepost prototype and a newly built function that returns a string message in the above format, but with unique data for each post. The constructor and all of your previous prototype additions are provided for you as a reference for property names and procedures. Remember that connectionsTo is a list of Fencepost objects.

--- buildingObject.js
+++ buildingObject.js
@@ -1,6 +1,39 @@
 /**
  * Created by Xiaolong on 4/1/14.
  */
-Fencepost.prototype.valueOf = function(){
-    return Math.sqrt(this.x*this.x + this.y*this.y);
+function Fencepost (x, y, postNum){
+    this.x = x;
+    this.y = y;
+    this.postNum = postNum;
+    this.connectionsTo = [];
+}
+Fencepost.prototype = {
+    sendRopeTo: function ( connectedPost ){
+        this.connectionsTo.push(connectedPost);
+    },
+    removeRope: function ( removeTo ){
+        var temp = [];
+        for(var i = 0; i<this.connectionsTo.length; i++){
+            if(this.connectionsTo[i].postNum != removeTo){
+                temp.push(this.connectionsTo[i]);
+            }
+        }
+        this.connectionsTo = temp;
+    },
+    movePost: function (x, y){
+        this.x = x;
+        this.y = y;
+    },
+    valueOf: function (){
+        return Math.sqrt( this.x*this.x + this.y*this.y );
+    }
+};
+
+Fencepost.prototype.toString = function(){
+    var list = "";
+    for(var i = 0; i < this.connectionsTo.length; i++){
+        list = list + this.connectionTo[i] + "\n";
+    }
+    return "Fence post #" + this.postNum + ":\nConnected to posts:\n" +
+        list + "Distance from ranch: " + this.valueOf() + " yards";
 };
